f114b1ee568ad8cf42c6dc3a0e1fcb8a08debb89
Camille Faulkner <camillecfaulkner@gmail.com>
2022-09-19 11:12:59 -0500

    commit

--- underground/settings.py
+++ underground/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://underground-cf.herokuapp.com/'
+    'https://underground-cf.herokuapp.com'
 )
 
 MIDDLEWARE = [
